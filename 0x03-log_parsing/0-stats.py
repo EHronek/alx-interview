dad16aa668f4f32e1cb4b8b6bc118b62d73218dc
EHronek <emmanuelhronek@gmail.com>
2025-05-08 22:24:07 +0300

    log file

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,56 +1,58 @@
 #!/usr/bin/python3
-"""Reads stdin line by line and computes metrics"""
+"""script that reads stdin line by line and computes metrics"""
 import sys
-import re
 
 
-def print_stats(status_codes, total_size):
-    """Prints the statistics"""
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
+def print_stats(dict_status_codes, t_file_size):
+    """
+    Method to print
+    Args:
+        dict_status_codes: dict of status codes
+        t_file_size: total of the file
+    Returns:
+        Nothing
+    """
 
-def main():
-    total_size = 0
-    status_codes = {
-        200: 0,
-        301: 0,
-        400: 0,
-        401: 0,
-        403: 0,
-        404: 0,
-        405: 0,
-        500: 0
-    }
-    line_count = 0
-
-    try:
-        for line in sys.stdin:
-            line_count += 1
-            match = re.match(
-                r'^\S+ - \[.*\] "GET /projects/260 HTTP/1\.1" (\d{3}) (\d+)$',
-                line.strip()
-            )
-            if not match:
-                continue
-
-            status = int(match.group(1))
-            file_size = int(match.group(2))
-
-            total_size += file_size
-            if status in status_codes:
-                status_codes[status] += 1
-
-            if line_count % 10 == 0:
-                print_stats(status_codes, total_size)
-
-    except KeyboardInterrupt:
-        print_stats(status_codes, total_size)
-        raise
-
-    print_stats(status_codes, total_size)
+    print("File size: {}".format(t_file_size))
+    for key, value in sorted(dict_status_codes.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
 
 
-if __name__ == "__main__":
-    main()
+t_file_size = 0
+code = 0
+counter = 0
+dict_status_codes = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0}
+
+try:
+    for line in sys.stdin:
+        # trimming
+        parsed_line = line.split()
+        # inverting
+        parsed_line = parsed_line[::-1]
+
+        if len(parsed_line) > 2:
+            counter += 1
+
+            if counter <= 10:
+                # file size
+                t_file_size += int(parsed_line[0])
+                # status code
+                code = parsed_line[1]
+
+                if (code in dict_status_codes.keys()):
+                    dict_status_codes[code] += 1
+
+            if (counter == 10):
+                print_stats(dict_status_codes, t_file_size)
+                counter = 0
+
+finally:
+    print_stats(dict_status_codes, t_file_size)
