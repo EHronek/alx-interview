d1a154c9af2cbb145d40dc54c7204a98db1776a3
EHronek <emmanuelhronek@gmail.com>
2025-05-22 22:09:11 +0300

    nqueens fix

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,44 +1,65 @@
 #!/usr/bin/python3
-""" The n queens algorithm"""
+"""The n queens algorithm"""
 import sys
 
+def is_safe(board, row, col, n):
+    # Check this row on left side
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    
+    # Check lower diagonal on left side
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    
+    return True
 
-if len(sys.argv) > 2 or len(sys.argv) < 2:
-    print("Usage: nqueens N")
-    exit(1)
+def solve_nqueens_util(board, col, n, solutions):
+    if col >= n:
+        solution = []
+        for i in range(n):
+            for j in range(n):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        solutions.append(solution)
+        return
+    
+    for i in range(n):
+        if is_safe(board, i, col, n):
+            board[i][col] = 1
+            solve_nqueens_util(board, col + 1, n, solutions)
+            board[i][col] = 0  # backtrack
 
-if not sys.argv[1].isdigit():
-    print("N must be a number")
-    exit(1)
+def solve_nqueens(n):
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    solutions = []
+    solve_nqueens_util(board, 0, n, solutions)
+    return solutions
 
-if int(sys.argv[1]) < 4:
-    print("N must be at least 4")
-    exit(1)
-
-n = int(sys.argv[1])
-
-
-def queens(n, idx=0, a=[], b=[], c=[]):
-    """ find possible positions """
-    if idx < n:
-        for j in range(n):
-            if j not in a and idx + j not in b and idx - j not in c:
-                yield from queens(n, idx + 1, a + [j], b + [idx + j], c + [idx - j])
-    else:
-        yield a
-
-
-def solve(n):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
-        k = []
-        i = 0
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    
+    solutions = solve_nqueens(n)
+    for solution in solutions:
+        print(solution)
 
 if __name__ == "__main__":
-    solve(n)
+    main()
