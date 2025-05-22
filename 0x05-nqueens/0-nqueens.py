8f6998646688f34811ee1ca0e4a15910168e485c
EHronek <emmanuelhronek@gmail.com>
2025-05-22 22:20:51 +0300

    nqueens fix

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,23 +2,25 @@
 """The n queens algorithm"""
 import sys
 
+
 def is_safe(board, row, col, n):
     # Check this row on left side
     for i in range(col):
         if board[row][i] == 1:
             return False
-    
+
     # Check upper diagonal on left side
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-    
+
     # Check lower diagonal on left side
     for i, j in zip(range(row, n, 1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-    
+
     return True
+
 
 def solve_nqueens_util(board, col, n, solutions):
     if col >= n:
@@ -29,12 +31,13 @@ def solve_nqueens_util(board, col, n, solutions):
                     solution.append([i, j])
         solutions.append(solution)
         return
-    
+
     for i in range(n):
         if is_safe(board, i, col, n):
             board[i][col] = 1
             solve_nqueens_util(board, col + 1, n, solutions)
             board[i][col] = 0  # backtrack
+
 
 def solve_nqueens(n):
     board = [[0 for _ in range(n)] for _ in range(n)]
@@ -42,24 +45,26 @@ def solve_nqueens(n):
     solve_nqueens_util(board, 0, n, solutions)
     return solutions
 
+
 def main():
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    
+
     try:
         n = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-    
+
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-    
+
     solutions = solve_nqueens(n)
     for solution in solutions:
         print(solution)
+
 
 if __name__ == "__main__":
     main()
